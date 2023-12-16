c065642d1bfa15ba58e8e1d4574edd979ad1503c
Jericho26 <88320876+Jericho26@users.noreply.github.com>
2023-12-16 12:09:28 +0800

    Add files via upload

---
+++ Folder/jericho_hospital.sql
@@ -0,0 +1,67 @@
+-- MySQL dump 10.13  Distrib 8.0.27, for Win64 (x86_64)
+--
+-- Host: localhost    Database: jericho
+-- ------------------------------------------------------
+-- Server version	8.0.35
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `hospital`
+--
+
+DROP TABLE IF EXISTS `hospital`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `hospital` (
+  `nameoftablet` varchar(45) DEFAULT NULL,
+  `Reference_No` int NOT NULL,
+  `dose` varchar(45) DEFAULT NULL,
+  `numberoftablet` varchar(45) DEFAULT NULL,
+  `lot` varchar(45) DEFAULT NULL,
+  `issuedate` varchar(45) DEFAULT NULL,
+  `expdate` varchar(45) DEFAULT NULL,
+  `dailydose` varchar(45) DEFAULT NULL,
+  `sideeffect` varchar(45) DEFAULT NULL,
+  `furtherinformation` varchar(45) DEFAULT NULL,
+  `bloodpressure` varchar(45) DEFAULT NULL,
+  `storageadvice` varchar(45) DEFAULT NULL,
+  `medication` varchar(45) DEFAULT NULL,
+  `patientid` varchar(45) DEFAULT NULL,
+  `nhsnumber` varchar(45) DEFAULT NULL,
+  `patientname` varchar(45) DEFAULT NULL,
+  `birthdate` varchar(45) DEFAULT NULL,
+  `patientaddress` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`Reference_No`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `hospital`
+--
+
+LOCK TABLES `hospital` WRITE;
+/*!40000 ALTER TABLE `hospital` DISABLE KEYS */;
+INSERT INTO `hospital` VALUES ('biogesic',12344,'500mg','10','22212','2222023','2222026','50mg','vomit, headache','cant sleep at night','80/120','n/a','biogesic only','12','34332','kim','12222000','Cuta, Batangas City'),('biogesic',123446,'500mg','10','22212','2222023','2222026','50mg','vomit, headache','cant sleep at night','80/120','n/a','biogesic only','12','34332','kim','12222000','Cuta, Batangas City');
+/*!40000 ALTER TABLE `hospital` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2023-12-16 10:16:41
